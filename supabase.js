5679ac71e31f57b359fe822dbc96a74a8a4e9711
fabioboschel-lang <fabioboschel@gmail.com>
2026-09-05 02:00:27 -0300

    Initialize Supabase client in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,18 @@
- 
+ import {
+  createClient
+} from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+
+const supabaseUrl =
+  "https://bdqcfzeuextdzmsapref.supabase.co";
+
+
+const supabaseKey =
+  "sb_publishable_Dvr9uhTqvLgLYwF4PUXmgA_Iyh3-dl3";
+
+
+export const supabase =
+  createClient(
+    supabaseUrl,
+    supabaseKey
+  );
